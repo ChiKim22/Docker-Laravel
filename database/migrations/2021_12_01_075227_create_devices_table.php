208d483dd2ed76280614e8ebbc796f595a142b13
ChiKim22 <wlsgh2505@gmail.com>
2021-12-01 19:01:41 +0900

    Laravel-Docker (Migration FIle)

---
+++ database/migrations/2021_12_01_075227_create_devices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('brand_id')->constrained()->onDelete('cascade');
+            $table->string('spec');
+            $table->year('release');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
