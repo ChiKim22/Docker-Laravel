ccfff706671d9b3452f86c0ac9ba971931f55065
ChiKim22 <wlsgh2505@gmail.com>
2021-12-06 20:37:19 +0900

    Laravel Term Project (+ Validate Error, Panigate)

---
+++ app/Http/Controllers/DeviceController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Device;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DeviceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Inertia::render('Device/Index', [
+            'devices' => Device::orderBy('id', 'desc')->paginate(6)->through(function ($device) {
+                return [
+                    'id' => $device->id,
+                    'image' => $device->image,
+                    'name' => $device->name,
+                    'brand' => $device->brand,
+                    'release' => $device->release,
+                    'spec' => $device->spec,
+                ];
+            }), // desc
+        ]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Device/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $now = now(); // Current year
+
+        $validated = $request->validate([
+            'brand' => 'required',
+            'spec' => 'required | min:1',
+            'release' => 'required |numeric|min:1900|max:'.($now->year+1),
+            'name' => 'required',
+            'image' => 'image',
+        ]);
+
+
+        // dd($request->all());
+        $path = $request->image->store('image', 'public');
+
+        $validated = array_merge($validated, ['image' => $path]);
+
+        $devices = Device::create($validated);
+        $devices->save();
+
+        return redirect()->route('devices.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Device  $device
+     * @return \Illuminate\Http\Response
+     */
+
+    public function show($id)
+    {
+        $devices = Device::find($id);
+        return Inertia::render('Device/Show', compact($device));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Device $device)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Device $device)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Device  $device
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Device $device, $id)
+    {
+        $devices = Device::find($id);
+        if($devices != null){
+            $devices->delete();
+            return redirect()->back();
+        }
+    }
+}
---
+++ resources/js/Pages/Device/Index.vue
@@ -0,0 +1,44 @@
+<template>
+    <app-layout>
+ <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+    <div v-for="device in devices.data" :key="device.id">
+    <div class="bg-gray-200 rounded overflow-hidden shadow-lg">
+        <div @click="show()">
+          <img :src="device.image" class="w-full h-auto" style="width: 400px; height: 250px; margin:0 auto; margin-top:20px" alt="image">
+        </div>
+
+      <div class="px-6 py-4">
+          <p class="font-bold text-xl mb-2">{{ device.brand }}</p>
+          <p class="font-bold text-gray-700 text-base">{{ device.name }}</p>
+      </div>
+
+      <div class="px-6 py-4 pb-2 bg-gray-700">
+          <span class="inline-block py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Released : {{ device.release }}</span>
+      </div>
+    </div>
+    </div>
+ </div>
+<pagination class="px-4 py-4" :links="devices.links" />
+    </app-layout>
+</template>
+
+<script>
+     import AppLayout from '@/Layouts/AppLayout.vue'
+     import Pagination from '@/Jetstream/Pagination.vue'
+
+export default {
+    props:['devices'],
+      components: {
+        AppLayout,
+        Pagination,
+      },
+  methods: {
+    show() {
+      this.$inertia.get('/device', this.devices.id)
+    },
+    submit() {
+      this.$inertia.post('/device', this.devices)
+    },
+  }
+}
+</script>
