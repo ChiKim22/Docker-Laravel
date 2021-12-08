47c27482275cc39c529f18688d40bd99d3e56152
ChiKim22 <wlsgh2505@gmail.com>
2021-12-09 03:29:54 +0900

    Laravel Term Project (Create, Index)

---
+++ resources/js/Pages/Device/Create.vue
@@ -0,0 +1,83 @@
+<template>
+    <app-layout>
+            <div class="py-12 px-4">
+                <div class=" max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                        <form class="px-4 py-4" @submit.prevent="registPhone"  enctype="multipart/form-data">
+                            <input type="hidden" name="_token" v-bind:value="csrf">
+                            <h1 class="font-bold text-xl mb-2">Regist Phone</h1>
+                        <div class="w-1/3 my-4">
+                            <label for="image" class="text-gray-700 select-none font-medium">Image</label>
+                            <input type="file" id="image" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200" @input="form.image = $event.target.files[0]">
+                        </div>
+                            <div v-if="errors.image">{{ errors.image }}</div>                        
+
+                        <div class="form-group my-2">
+                        <label for="brand">Brand : </label>
+                        <select class="form-control" id="brand" placeholder="Brand" v-model="this.form.selected" style="width:30%" >
+                            <option>Apple</option>
+                            <option>Samsung</option>
+                            <option>Google</option>
+                            <option value="other">Other Brands</option>
+                        </select>
+                                <input  v-if="this.form.selected =='other'" type="text" placeholder="Brand" v-model="this.form.brand" >
+                                <input v-else-if="this.form.selected != 'other'" v-show="this.form.brand = this.form.selected">
+                        </div>
+                            <div v-if="errors.brand">{{ errors.brand }}</div>
+
+                        <div class="form-group my-2">
+                        <label for="name">Name : </label>
+                        <input type="text" class="form-control" id="name" placeholder="Name" v-model="this.form.name" style="width:30%" >
+                        </div>
+                            <div v-if="errors.name">{{ errors.name }}</div>
+
+                        <div class="form-group my-2">
+                        <label for="release">release : </label>
+                        <input type="number" class="form-control" id="release" placeholder="Release" v-model="this.form.release" style="width:30%" >
+                        </div>
+                            <div v-if="errors.release">{{ errors.release }}</div>
+
+                        <div class="form-group my-2">
+                        <label for="spec">Spec : </label>
+                        <input type="text" class="form-control" id="spec" placeholder="Spec" v-model="this.form.spec" style="width:30%" >
+                        </div>
+                            <div v-if="errors.spec">{{ errors.spec }}</div>
+
+                        <button type="submit" class="btn btn-primary font-bold text-xl mx-2 my-2">Regist</button>
+                        <a class="font-bold text-xl mx-2 my-2" href="/devices" >Back</a>
+                        </form>
+                     </div>
+                </div>
+             </div>
+        </app-layout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout.vue'
+import { useForm } from '@inertiajs/inertia-vue3'
+
+export default {
+    setup () {
+    const form = useForm({
+        selected: '',
+        image: null,
+        name: '',
+        release: null,
+        brand: '',
+        spec : '',
+    })
+    function registPhone() {
+              form.post('/devices', this.form)
+          }
+    return { form, registPhone }
+  },
+  components: {
+      AppLayout,
+},
+      props: [
+        'errors',
+        'devices',
+        'csrf',
+      ],
+}
+</script>
--- resources/js/Pages/Device/Index.vue
+++ resources/js/Pages/Device/Index.vue
@@ -1,20 +1,47 @@
 <template>
     <app-layout>
  <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-    <div v-for="device in devices.data" :key="device.id">
+    <div v-for="device in devices.data" :key="device.id" >
     <div class="bg-gray-200 rounded overflow-hidden shadow-lg">
-        <div @click="show()">
-          <img :src="device.image" class="w-full h-auto" style="width: 400px; height: 250px; margin:0 auto; margin-top:20px" alt="image">
-        </div>
+      <div>
+        <!-- <button @click="openModal()" :props="device" class="w-full h-auto"  > -->
+           <button @click="show(device)"  class="w-full h-auto" >
+              <img :src="device.image" style="width: 100%; height: 300px; margin:0 auto; margin-top:20px" alt="image">
+          </button>
+      </div>
 
       <div class="px-6 py-4">
           <p class="font-bold text-xl mb-2">{{ device.brand }}</p>
           <p class="font-bold text-gray-700 text-base">{{ device.name }}</p>
       </div>
 
-      <div class="px-6 py-4 pb-2 bg-gray-700">
+      <div class="px-6 py-4 pb-2">
           <span class="inline-block py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Released : {{ device.release }}</span>
       </div>
+
+
+<!-- This element is to trick the browser into centering the modal contents. -->
+  <!-- <dialog-modal :show="isOpen == true" >
+    <template #title>
+      <input class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+				id="name"	readonly/> 
+        <h1 v-bind="device">{{ device.name }}</h1>
+    </template>
+
+    <template #content>
+            <input class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+				id="spec"	v-model="form.spec" readonly />
+    </template>
+
+    <template #footer>
+             <button @click="closeModal()">
+                Close
+            </button>
+    </template>
+</dialog-modal> -->
+
+
+
     </div>
     </div>
  </div>
@@ -23,22 +50,105 @@
 </template>
 
 <script>
-     import AppLayout from '@/Layouts/AppLayout.vue'
-     import Pagination from '@/Jetstream/Pagination.vue'
+    import AppLayout from '@/Layouts/AppLayout.vue'
+    import Pagination from '@/Jetstream/Pagination.vue'
+    // import DialogModal from '@/Jetstream/DialogModal.vue'
+    // import { computed } from '@vue/runtime-core'
+    // import { usePage } from '@inertiajs/inertia-vue3'
 
 export default {
     props:['devices'],
+    setup() {
+        // const device = computed(() => usePage().props.value.device )
+        // return {device}
+    },
       components: {
         AppLayout,
         Pagination,
       },
+      data(){
+        return{
+          // isOpen: false,
+        }
+      },
   methods: {
-    show() {
-      this.$inertia.get('/device', this.devices.id)
-    },
-    submit() {
-      this.$inertia.post('/device', this.devices)
+
+  // openModal: function(){
+  //   this.isOpen = true;
+
+    // this.form.name = this.devices.name;
+    // this.form.spec = this.devices.spec;
+    // update();
+
+  // },
+  //   update: function(){
+  //   this.$inertia.get('/devices/'+ devices.id)
+  // },
+  // closeModal: function(){
+  //   this.isOpen = false;
+  //   this.reset();
+  //   this.editMode=false;
+  // },
+  // reset: function(){
+  //   this.form = {
+  //       id: this.devices,
+  //       image: null,
+  //       name: '',
+  //       release: null,
+  //       brand: '',
+  //       spec : '',
+  //   }
+  // },
+
+    show: function(devices) {
+      this.$inertia.get('/devices/' + devices.id)
     },
   }
 }
 </script>
+
+<style>
+body {
+  background: #0077ff;
+  font-size: 62.5%;
+}
+
+.container {
+  padding: 2em;
+}
+
+button,
+button::after {
+  -webkit-transition: all 0.3s;
+    -moz-transition: all 0.3s;
+  -o-transition: all 0.3s;
+    transition: all 0.3s;
+}
+
+button {
+  background: none;
+  border: 3px solid #fff;
+  border-radius: 5px;
+  color: #fff;
+  display: block;
+  font-size: 1.6em;
+  font-weight: bold;
+  margin: 1em auto;
+  padding: 2em 6em;
+  position: relative;
+  text-transform: uppercase;
+}
+
+button::before,
+button::after {
+  background: #fff;
+  content: '';
+  position: absolute;
+  z-index: -1;
+}
+
+button:hover {
+  color: #0077ff;
+}
+
+</style>
